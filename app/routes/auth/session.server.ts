2c17aff85338d731f99638942334767176e1e5ce
Kannavi Mishra <79000269+cloudcreatr@users.noreply.github.com>
2023-12-27 07:34:53 +0000

    Update session storage and cookie configuration

--- app/routes/auth/session.server.ts
+++ app/routes/auth/session.server.ts
@@ -1,4 +1,8 @@
-import { createCookieSessionStorage } from "@remix-run/cloudflare"; // or cloudflare/deno
+import {
+  createCookie,
+  createWorkersKVSessionStorage,
+  createCookieSessionStorage,
+} from "@remix-run/cloudflare"; // or cloudflare/deno
 
 type SessionData = {
   verifier: string;
@@ -7,25 +11,38 @@ type SessionData = {
   id_token: string;
 };
 
-type SessionFlashData = {
-  error: string;
-};
+const sessionCookie = createCookie("__session", {
+  domain: "cloudcreatr.com",
+  httpOnly: true,
+  maxAge: 7200,
+  path: "/",
+  sameSite: "lax",
+  secrets: ["c32ba072c91c4a85c0ab982ed9cab0fc133e552e606aeee6c93d98d8a583f28a"],
+  secure: true,
+});
 
 const { getSession, commitSession, destroySession } =
-  createCookieSessionStorage<SessionData, SessionFlashData>({
-    cookie: {
-      name: "__session",
-      domain: "cloudcreatr.com",
-
-      httpOnly: true,
-      maxAge: 7200,
-      path: "/",
-      sameSite: "lax",
-      secrets: [
-        "c32ba072c91c4a85c0ab982ed9cab0fc133e552e606aeee6c93d98d8a583f28a",
-      ],
-      secure: true,
-    },
+  createWorkersKVSessionStorage<SessionData>({
+    // The KV Namespace where you want to store sessions
+    kv: SESSION_KV,
+    cookie: sessionCookie,
   });
+
+// const { getSession, commitSession, destroySession } =
+//   createCookieSessionStorage<SessionData>({
+//     cookie: {
+//       name: "__session",
+//       domain: "cloudcreatr.com",
+
+//       httpOnly: true,
+//       maxAge: 7200,
+//       path: "/",
+//       sameSite: "lax",
+//       secrets: [
+//         "c32ba072c91c4a85c0ab982ed9cab0fc133e552e606aeee6c93d98d8a583f28a",
+//       ],
+//       secure: true,
+//     },
+//   });
 
 export { getSession, commitSession, destroySession };
